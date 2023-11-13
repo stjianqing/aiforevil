9ccdaba03f12b40e9f7fb9811be52305afbff7cd
valudollar <valerie.luxuanhui@gmail.com>
2023-11-13 20:19:24 +0800

    coordinates and date template

--- my-nextjs-app/src/components/header.tsx
+++ my-nextjs-app/src/components/header.tsx
@@ -1,8 +1,16 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 export const Header: React.FC = () => {
+  const router = useRouter();
+  function handleHome() {
+    router.push("/");
+  }
+
   return (
     <div className="w-screen h-[4rem] flex justify-center absolute">
       <div className="w-[95%] h-[4rem] flex items-center text-[2rem] border-b-2 border-[#969696]">
-        ForestFind
+        <button onClick={handleHome}>ForestFind</button>
       </div>
     </div>
   );
--- my-nextjs-app/src/pages/findforest/coordinates.tsx
+++ my-nextjs-app/src/pages/findforest/coordinates.tsx
@@ -1,18 +1,56 @@
 import React, { useState, createRef, useEffect } from "react";
-// import Cropper, { ReactCropperElement } from "react-cropper";
-// import "cropperjs/dist/cropper.css";
-import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 
-// const defaultSrc = "/small.jpg";
+export const FFCoordinate: React.FC = () => {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const router = useRouter();
+  function handleLatitudeChange(e) {
+    setLatitude(e.target.value);
+  }
+  function handleLongitudeChange(e) {
+    setLongitude(e.target.value);
+  }
 
-export const Home: React.FC = () => {
-  function handleFindForest() {}
-
-  function handleForestChange() {}
+  function handleConfirm() {
+    router.push({
+      pathname: "/findforest/date",
+      query: { latitude: latitude, longitude: longitude },
+    });
+  }
   return (
-    <div className="flex w-screen h-screen flex-col justify-center items-center  "></div>
+    <div className="flex w-screen h-screen flex-col pt-[5rem] items-center  ">
+      <h1 className="text-[1.5rem]">Find Forest</h1>
+
+      <div className="flex w-1/2 flex-col justify-center items-center">
+        <p>Enter your coordinates</p>
+        <div className="flex flex-row mt-[1rem]">
+          <div className="flex flex-col mr-[2rem]">
+            <label className="mt-1 mb-6">Latitude:</label>
+            <label className="">Longitude:</label>
+          </div>
+          <div className="flex flex-col">
+            <input
+              onChange={handleLatitudeChange}
+              type="text"
+              className="w-[8rem] mb-4 border border-black p-1 rounded "
+            ></input>
+            <input
+              onChange={handleLongitudeChange}
+              type="text"
+              className="w-[8rem] border border-black p-1 rounded"
+            ></input>
+          </div>
+        </div>
+        <button
+          onClick={handleConfirm}
+          className="mt-[1rem] border border-black rounded-xl p-2"
+        >
+          Confirm
+        </button>
+      </div>
+    </div>
   );
 };
 
-export default Home;
+export default FFCoordinate;
