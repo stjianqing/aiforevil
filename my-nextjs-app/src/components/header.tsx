7f8d29b426bcdaab80601e1cfd6ad855771c95a7
valudollar <valerie.luxuanhui@gmail.com>
2023-11-14 15:46:04 +0800

    crop tool template

--- my-nextjs-app/src/components/header.tsx
+++ my-nextjs-app/src/components/header.tsx
@@ -8,7 +8,7 @@ export const Header: React.FC = () => {
   }
 
   return (
-    <div className="w-screen h-[4rem] flex justify-center absolute">
+    <div className="w-screen h-[4rem] flex justify-center absolute bg-transparent">
       <div className="w-[95%] h-[4rem] flex items-center text-[2rem] border-b-2 border-[#969696]">
         <button onClick={handleHome}>ForestFind</button>
       </div>
