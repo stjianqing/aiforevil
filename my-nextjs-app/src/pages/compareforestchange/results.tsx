517b99787770bc91361be7cacd52f480f8cd6131
Abzzt <saxaphoneviolin@gmail.com>
2023-11-16 16:03:26 +0800

    Added download pdf feature

--- my-nextjs-app/src/pages/compareforestchange/results.tsx
+++ my-nextjs-app/src/pages/compareforestchange/results.tsx
@@ -1,7 +1,8 @@
-import React, { useState, createRef, useEffect } from "react";
+import React, { useState, createRef, useEffect, useRef} from "react";
 import { useRouter } from "next/router";
 const defaultSrc = "/small.jpg";
 import Image from "next/image";
+import html2pdf from 'html2pdf.js';
 
 export const FCResults: React.FC = () => {
   const router = useRouter();
@@ -9,10 +10,40 @@ export const FCResults: React.FC = () => {
   const [longitude, setLongitude] = useState(router.query.longitude);
   const [date1, setDate1] = useState(router.query.date1);
   const [date2, setDate2] = useState(router.query.date2);
+  const fileName: string = "report.pdf"
+  const [isVisible, setIsVisible] = useState(false);
 
-  function handleConfirm() {
-    router.push("/index");
-  }
+  const htmlContent = `
+    <div>
+      <h2 style="font-size: 2rem; text-align:center; font-family:system-ui; ">Satellite Image in ${date1} and ${date2}</h2>
+      <p style="font-size: 1.5rem; text-align:center; font-familt: system-ui;">Location: ${latitude}, ${longitude}</p>
+      <Image style="display: block; margin-left: auto;margin-right: auto; width: 70%; alignment:center; margin-top: 1.5rem" src="${defaultSrc}" alt="Cropped image" width={500} height={500} justify: center></Image>
+    </div>`;
+
+  const DownloadPDFButton = ({}) => {
+    const containerRef = useRef(null);
+    const handleDownload = () => {
+      const element = containerRef.current;
+      const opt =  {
+        margin: 10,
+        filename: "Report.pdf",
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+      }
+      html2pdf().set(opt).from(htmlContent).save();
+    };
+
+    return (
+      <div>
+        <button 
+          onClick={handleDownload}
+          className="mt-[1rem] border border-black rounded-xl p-2">Download</button>
+
+      </div>
+    )
+  };
+
 
   return (
     <div className="flex w-screen h-screen flex-col pt-[5rem] items-center ">
@@ -27,8 +58,10 @@ export const FCResults: React.FC = () => {
           <p>Date 1: {date1}</p>
           <p>Date 2: {date2}</p>
         </div>
+        <div>
+          <DownloadPDFButton />
+        </div> 
       </div>
-      
     </div>
   );
 };
