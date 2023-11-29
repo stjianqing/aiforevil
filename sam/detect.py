610f7bbbf57d0127dba781f9c528b4c2540c923c
ry2001 <ruiyangtan15@gmail.com>
2023-11-29 15:17:50 +0800

    code refactoring

--- sam/detect.py
+++ sam/detect.py
@@ -2,21 +2,18 @@
 Using Segment Anything Model to detect the edge of the image.
 
 Model checkpoint download: https://github.com/facebookresearch/segment-anything#model-checkpoints
+Recommend to use vit_l
 
 Input:
     sam_checkpoint: the checkpoint of the model
     model_type: the type of the model
     img_path: the path of the image
     size: the size of the image, reduce if cannot process (default: (1024x1024))
-    edge: whether to get the edge of the image (default: True)
     segment: whether to get the segmentated image (default: True)
-    get_max: whether to get the max area of the image (default: True)
 
 Output:
-    edge.png: the edge of the image
-    segmentation.png: the segmentated image
-    overlay_edge.png: the overlay of the edge on the original image
-    overlay_segment.png: the overlay of the segmentated image on the original image
+    overlay.png: the overlay of the segmentated image on the original image
+    output.shp.zip: the shapefile of the segmentated image if it is a TIF file
 '''
 import numpy as np
 import torch
@@ -31,15 +28,13 @@ sys.path.append("..")
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 
 class EdgeDetector():
-    def __init__(self, checkpoint, model_type, img_path, size=(1024, 1024), edge=True, segment=True, get_max=True):
+    def __init__(self, checkpoint, model_type, img_path, size=(1024, 1024), segment=True):
         self.checkpoint = checkpoint
         self.model_type = model_type
         self.img_path = img_path
         self.TIF = self.img_path.endswith('.tif')
         self.size = size
-        self.edge = edge
         self.segment = segment
-        self.get_max = get_max
         self.schema = {
                 'geometry': 'Polygon',
                 'properties': {'id': 'int'},
@@ -59,12 +54,30 @@ class EdgeDetector():
         self.predictor = SamPredictor(sam)
 
     def _preprocess(self):
+        """
+        Preprocesses the image before performing any operations.
+
+        This function reads the image from the specified image path using OpenCV.
+        If the image is not found, it raises a ValueError.
+        The image is then converted from BGR to RGB color format.
+        The original image is stored as a copy for reference.
+        The size of the image is determined and compared to the specified size.
+        If the image is larger, it is resized to the specified size.
+        The image is passed to the predictor for further processing.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         # enable opencv to read jp2 files
         os.environ["OPENCV_IO_ENABLE_JASPER"] = "true"
         self.img = cv2.imread(self.img_path)
         if self.img is None:
             raise ValueError("Image not found.")
         self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.original_img = self.img.copy()
         self.img_size = (self.img.shape[1], self.img.shape[0])
 
         if self.img.shape > self.size:
@@ -79,53 +92,60 @@ class EdgeDetector():
         '''
         self.input_point = np.array([
                                         [self.img_size[0]//2, self.img_size[1]//2],
-                                        # [0, 0],
-                                        # [self.img_size[0], 0],
-                                        # [self.img_size[0], self.img_size[1]],
-                                        # [0, self.img_size[1]]
                                     ])
+        self.point_label = np.array([1]) # Labels: 1 is add, 0 is remove
 
     def _process(self):
+        """
+        Process the image by applying segmentation and generating an overlay.
+
+        This function takes no arguments and returns no values.
+
+        Steps:
+        1. Use the predictor to predict masks for the input point coordinates and labels.
+        2. Create an empty segmentation image.
+        3. Set the color of the segmentation image for the predicted masks.
+        4. Resize the segmentation image to match the size of the input image.
+        5. Convert the segmentation image to grayscale.
+        6. Apply a threshold to the grayscale image to generate an alpha channel.
+        7. Find contours in the grayscale image.
+        8. Draw filled contours on the alpha channel.
+        9. Convert the original image to RGBA format.
+        10. Iterate over each pixel in the alpha channel and set the corresponding pixel in the image to blue.
+        11. Save the image as "overlay.png".
+        12. If it is a TIF file, generate a shapefile with the coordinates.
+        """
         masks, _, _ = self.predictor.predict(
             point_coords=self.input_point,
-            # point_labels=np.array([1, 0, 0, 0, 0]), # labels: 1 is add, 0 is remove
-            point_labels=np.array([1]), # labels: 1 is add, 0 is remove
+            point_labels=self.point_label, 
             multimask_output=True,
         )
-        # masks = self.mask_generator.generate(self.img)
-        # # only getting the max area
-        # if self.get_max:
-        #     masks = self.max_area(masks)
-        segmentation = self.img.copy()
-        for col in range(self.img.shape[1]):
-            for row in range(self.img.shape[0]):
-                # NOTE: need to figure out which is the best
-                if masks[1][col][row] == True:
-                    segmentation[col][row] = [255, 0, 0]
-                else:
-                    segmentation[col][row] = [0, 0, 0]
-        # get segmentated image
-        if self.segment:
-            tmp = cv2.cvtColor(segmentation, cv2.COLOR_RGB2GRAY)
-            _, alpha = cv2.threshold(tmp, 0, 128, cv2.THRESH_BINARY)
-            b, g, r = cv2.split(segmentation)
-            rgba = [r, g, b, alpha]
-            segment = cv2.merge(rgba, 4)
-            segment = cv2.resize(segment, self.img_size)
-            cv2.imwrite("segmentation.png", segment)
-            self.overlay(segment, 'segment')
-        # from the mask, get the edge
-        if self.edge:
-            edge = cv2.Canny(segmentation, 0, 1)
-            edge = cv2.resize(edge, self.img_size)
-            _, alpha = cv2.threshold(edge, 0, 255, cv2.THRESH_BINARY)
-            if self.TIF:
-                self._generate_shapefile(alpha)
-            rgba = [edge, edge, edge, alpha]
-            edge = cv2.merge(rgba,4)
-            cv2.imwrite("edge.png", edge)
-            self.overlay(edge, 'edge')
 
+        segmentation = np.zeros_like(self.img)
+        segmentation[masks[1]] = [255, 0, 0]
+
+        segmentation = cv2.resize(segmentation, self.img_size)
+
+        gray = cv2.cvtColor(segmentation, cv2.COLOR_BGR2GRAY)
+        _, alpha = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            if len(contour) <= 1000:
+                cv2.drawContours(alpha, [contour], -1, (0, 0, 0, 100), thickness=cv2.FILLED)
+
+        image = cv2.cvtColor(self.original_img, cv2.COLOR_BGR2RGBA)
+        for col in range(alpha.shape[0]):
+            for row in range(alpha.shape[1]):
+                if alpha[col][row] == 255:
+                    image[col][row] = [0, 0, 255, 255]
+
+        cv2.imwrite(f"overlay.png", image)
+
+        if self.TIF:
+            self._generate_shapefile(alpha, contours)
+
+    # this function is only being used when we use automated mask
     def max_area(self, masks):
         sorted_masks = sorted(masks, key=lambda x: x['area'], reverse=True)
         # NOTE: there might have chances where the forest is not the largest area of the image
@@ -133,57 +153,42 @@ class EdgeDetector():
         i = area.index(max(area))
         return sorted_masks[0]['segmentation']
 
-    def overlay(self, overlay, type):
-        image = self.img.copy()
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-        image = cv2.resize(image, self.img_size)
-        cv2.addWeighted(overlay, 1, image, 1, 0, image)
-        image = cv2.resize(image, self.img_size)
-        cv2.imwrite(f"overlay_{type}.png", image)
-
-    def _generate_shapefile(self, edge):
+    def _generate_shapefile(self, segment, contours):
+        """
+        Generates a shapefile based on the given segment value and contours.
+        
+        Parameters:
+            segment (numpy.ndarray): A 2D array representing the segmentation.
+            contours (list): A list of contours to generate the shapefile from.
+        
+        Returns:
+            None
+        """
         raster = rasterio.open(self.img_path)
         array = raster.read(1)
         height, width = array.shape
 
-        # Create two arrays with the same shape as the input array/raster, where each value is the x or y index of that cell
         cols, rows = np.meshgrid(np.arange(width), np.arange(height))
-
-        # Create two arrays with the same shape as the input array/raster, where each value is the x or y coordinate of that cell
         xs, ys = rasterio.transform.xy(raster.transform, rows, cols)
 
-        # Convert the coordinate lists to arrays
-        xcoords = np.array(xs)
-        ycoords = np.array(ys)
+        x_coords = np.array(xs)
+        y_coords = np.array(ys)
 
-        # Find contours in the binary image
-        contours, _ = cv2.findContours(edge, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+        longitudes = x_coords * segment / 255.0
+        latitudes = y_coords * segment / 255.0
 
-        # get the pixel latitude and longitude
-        long = xcoords * edge / 255.0
-        lat = ycoords * edge / 255.0
-
-        # store all the polygons
         polygons = []
 
-        for _, contour in enumerate(contours):
-            c = []
-            # Convert contour to polygon
-            if len(contour) >= 4:
+        for contour in contours:
+            if len(contour) >= 1000:
                 squeezed = contour.squeeze()
-                # replace the pixel coordinates to latitude and longitude
-                for i in range(len(squeezed)):
-                    x, y = squeezed[i][0], squeezed[i][1]
-                    c.append([long[y][x], lat[y][x]])
-                polygon = Polygon(c)
+                points = [[longitudes[y][x], latitudes[y][x]] for x, y in squeezed]
+                polygon = Polygon(points)
                 polygons.append(polygon)
 
-        multipolygon = MultiPolygon(polygons)  # putting it all together in a MultiPolygon
+        multipolygon = MultiPolygon(polygons)
 
-        # save the output shapefile as zip file
-        with fiona.open('output_test.shp.zip', 'w', 'ESRI Shapefile', self.schema) as c:
-        # with fiona.open('output_test.shp', 'w', 'ESRI Shapefile', self.schema) as c:
-            # Write polygon to shapefile
+        with fiona.open('output.shp.zip', 'w', 'ESRI Shapefile', self.schema) as c:
             c.write({
                 'geometry': mapping(multipolygon),
                 'properties': {'id': 0},
@@ -199,6 +204,7 @@ class EdgeDetector():
 if __name__ == "__main__":
     sam_checkpoint = "./model/sam_vit_l_0b3195.pth"
     model_type = "vit_l"
+    # img_path = "C:/Users/ruiya/Downloads/s2_sr_median_export (12).tif" # cat tien
 
     img_path = "C:/Users/ruiya/Downloads/s2_sr_median_export (17).tif" # cuc phuong
     edge_detector = EdgeDetector(sam_checkpoint, model_type, img_path)
